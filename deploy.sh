cb66eb05d8ae02d029ba6e0763103c5fd97bacb6
wangchenqing <wangchenqing@100.me>
2022-08-02 10:29:35 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,9 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+# 安装依赖文件
+npm install
+
 # 生成静态文件
 npm run build
 
@@ -14,6 +17,6 @@ git add .
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:WangChenQing94/doc.git release:release
+git push -f git@github.com:WangChenQing94/doc.git master:release
 
 cd -
